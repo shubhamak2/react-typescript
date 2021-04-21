f9eaeb04ce73527fe43238f1ee2791cd60badd3b
bharat <bhk2705@gmail.com>
2021-04-21 12:40:53 +0530

    Modified repo styles.

--- src/components/userDetails/userRepo.tsx
+++ src/components/userDetails/userRepo.tsx
@@ -6,7 +6,7 @@ import * as styles from '../../styles';
 export const UserRepo = ({ repoName }: any) => {
   return (
     <div style={{ display: 'flex' }}>
-      <span>{repoName}</span>
+      <span>&#8226;</span>&nbsp;<span>{repoName}</span>
     </div>
   );
 };
